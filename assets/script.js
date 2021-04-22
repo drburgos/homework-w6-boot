c6562340b9b9d9a91db111ba7b511e2547a5f0a3
Joaquin Burgos <joaquin.burgos@gmx.com>
2021-04-22 10:46:19 -0500

    js fetch

--- assets/script.js
+++ assets/script.js
@@ -4,22 +4,20 @@
 // 1.1 Define object to fetch text
 // 1.2 Define object to fetch button
 
-var enterCity = document.getElementById("textInput");
+var cityInput = document.getElementById("textInput");
 var searchButton = document.getElementById("searchButton");
 
-let CityValue = enterCity.value;
-
 // 1.3 Create function to fetch input when button is clicked
-function getInfoFromSearchForm (event) {
+function handleClickSearchBtn (event) {
 
     // 1.4 Prevent default behavior of the form
     event.preventDefault();
 
     // 1.5 Create variable to store what was fetched
-    var CityValue = enterCity.value;
+    var cityName = cityInput.value;
 
     // 1.6 If statement in case no value is inputted 
-        if (CityValue === "") {
+        if (cityName === "") {
 
             //2.6.1 Error statement
             window.alert ("You need to enter a City.");
@@ -27,43 +25,44 @@ function getInfoFromSearchForm (event) {
             //2.6.2 End of if statement
             return;
         }
-    // 1.7 call function to fetch weather information
-        else {
-                
-        getWeatherInformation(CityValue);
+       
+        getWeatherInformation(cityName);
 
-        }
-            
 };
 
-console.log(CityValue);
+
 
 // 2. Define event listner on button
 
-searchButton.addEventListener("click", getInfoFromSearchForm);
+searchButton.addEventListener("click", handleClickSearchBtn);
 
 // 3. Proceed to get weather information
 
-function getWeatherInformation (CityValue) {
+function getWeatherInformation (cityName) {
 
     // 3.1 Define all things you need to fetch the info.
     // 3.1.1 You need a key
-    var key = "cbbb3e31bb56d531f65d4b1b75f9df4e";
+    const API_KEY = "cbbb3e31bb56d531f65d4b1b75f9df4e";
     // 3.1.2 You need a URL
-    // NOTE to person who is grading: I know that the excercise requires to go to the one call API (https://openweathermap.org/api/one-call-api). However it returns latitude and longitude which I have no idea how to transform into a city. On the other hand, the following Openweather API, returns the current weather of the city that I need, which is what I want for the excercise.
-    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+    // NOTE to person who is grading: I know that the excercise requires to go to the one call API (https://openweathermap.org/api/one-call-api). However it returns latitude and longitude. That is not enough information so I also added Openweather API, which returns the current weather of the city that I need, using the same key which is what I want for the excercise.
+    // To handle the API, I will be breaking it up...
+    const BASE_URL = "http://api.openweathermap.org/data/2.5/";
+    // saving request url to a variable
+    let requestUrl = BASE_URL+ "/weather?q=";
     
     // 3.1.3 Modifying variable to include the city that was entered.
-    requestUrl= requestURL + CityValue + "&appid=" + key;
+    requestUrl= requestUrl + cityName + "&appid=" + API_KEY;
 
     // 3.1.4. Adding fetch function
     fetch(requestUrl)
-        //Remote server gives response and stores it in parameter
+        //Remote server gives response (promise) and stores it in parameter
         .then(function (response) {
-            //Parameter is transformed into a json object
-            return response.json();
+            //Parameter (promise) is transformed into a json object
+            return response.json(); 
+
+            
         })
 
-
+        
 
 }
